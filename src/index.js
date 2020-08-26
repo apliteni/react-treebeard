319c2d134e4ec9fab9fc81f22d2a8bd378de4d95
Alexander Naumtcev <38173482+naumtcev@users.noreply.github.com>
2020-08-26 17:05:15 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Treebeard from "./components/Treebeard/";
-import decorators from "./components/Decorators/Decorators";
+import Treebeard from "./components/";
+import decorators from "./components/Decorators";
 import theme from "./themes/default";
 
 export { Treebeard, decorators, theme };
