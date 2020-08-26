6db8fe6db79a224a2a9be49c84adb258fa638889
Alexander Naumtcev <38173482+naumtcev@users.noreply.github.com>
2020-08-26 15:48:47 +0700

    Update Container.jsx

--- src/components/Decorators/Container.jsx
+++ src/components/Decorators/Container.jsx
@@ -1,38 +1,47 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 class Container extends PureComponent {
-    renderToggleDecorator() {
-        const {style, decorators, onClick} = this.props;
-        return <decorators.Toggle style={style.toggle} onClick={onClick}/>;
-    }
+  handleClickHead = (e) => {
+    const { onSelect, onClick } = this.props;
+    onSelect(e);
+    onClick(e);
+  };
 
-    render() {
-        const {
-            style, decorators, terminal, node, onSelect, customStyles
-        } = this.props;
-        return (
-            <div style={node.active ? {...style.container} : {...style.link}}>
-                {!terminal ? this.renderToggleDecorator() : null}
-                <decorators.Header node={node} style={style.header} customStyles={customStyles} onSelect={onSelect}/>
-            </div>
-        );
-    }
+  renderToggle() {
+    const { style, decorators, onClick } = this.props;
+    return <decorators.Toggle style={style.toggle} onClick={onClick} />;
+  }
+
+  render() {
+    const { style, decorators, terminal, node, customStyles } = this.props;
+    return (
+      <div style={node.active ? { ...style.container } : { ...style.link }}>
+        {!terminal ? this.renderToggle() : null}
+        <decorators.Header
+          node={node}
+          style={style.header}
+          customStyles={customStyles}
+          onSelect={(e) => this.handleClickHead(e)}
+        />
+      </div>
+    );
+  }
 }
 
 Container.propTypes = {
-    customStyles: PropTypes.object,
-    style: PropTypes.object.isRequired,
-    decorators: PropTypes.object.isRequired,
-    terminal: PropTypes.bool.isRequired,
-    onClick: PropTypes.func.isRequired,
-    onSelect: PropTypes.func,
-    node: PropTypes.object.isRequired
+  customStyles: PropTypes.object,
+  style: PropTypes.object.isRequired,
+  decorators: PropTypes.object.isRequired,
+  terminal: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+  onSelect: PropTypes.func,
+  node: PropTypes.object.isRequired,
 };
 
 Container.defaultProps = {
-    onSelect: null,
-    customStyles: {}
+  onSelect: null,
+  customStyles: {},
 };
 
 export default Container;
