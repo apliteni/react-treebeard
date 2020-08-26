3747b24e5abaea7830dfa89db6c769d4b994f1ed
Alexander Naumtcev <38173482+naumtcev@users.noreply.github.com>
2020-08-26 15:46:41 +0700

    Update NodeHeader.jsx

--- src/components/NodeHeader.jsx
+++ src/components/NodeHeader.jsx
@@ -1,47 +1,51 @@
-import React, {Component} from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import shallowEqual from 'shallowequal';
-import deepEqual from 'deep-equal';
 
-class NodeHeader extends Component {
-
-    render() {
-        const {
-            decorators, node, onClick, style, onSelect, customStyles
-        } = this.props;
-        const {active, children} = node;
-        const terminal = !children;
-        let styles;
-        if (active) {
-            styles = Object.assign(style, {container: {...style.link, ...style.activeLink}});
-        } else {
-            styles = style;
-        }
-        return (
-            <decorators.Container
-                decorators={decorators}
-                node={node}
-                onClick={onClick}
-                customStyles={customStyles}
-                onSelect={onSelect}
-                terminal={terminal}
-                style={styles}
-            />
-        );
+class NodeHeader extends PureComponent {
+  render() {
+    const {
+      decorators,
+      node,
+      onClick,
+      style,
+      onSelect,
+      customStyles,
+    } = this.props;
+    const { active, children } = node;
+    const terminal = !children;
+    let styles;
+    if (active) {
+      styles = Object.assign(style, {
+        container: { ...style.link, ...style.activeLink },
+      });
+    } else {
+      styles = style;
     }
+    return (
+      <decorators.Container
+        decorators={decorators}
+        node={node}
+        onClick={onClick}
+        customStyles={customStyles}
+        onSelect={onSelect}
+        terminal={terminal}
+        style={styles}
+      />
+    );
+  }
 }
 
 NodeHeader.propTypes = {
-    style: PropTypes.object.isRequired,
-    customStyles: PropTypes.object,
-    decorators: PropTypes.object.isRequired,
-    node: PropTypes.object.isRequired,
-    onClick: PropTypes.func,
-    onSelect: PropTypes.func
+  style: PropTypes.object.isRequired,
+  customStyles: PropTypes.object,
+  decorators: PropTypes.object.isRequired,
+  node: PropTypes.object.isRequired,
+  onClick: PropTypes.func,
+  onSelect: PropTypes.func,
 };
 
 NodeHeader.defaultProps = {
-    customStyles: {}
+  customStyles: {},
 };
 
 export default NodeHeader;
