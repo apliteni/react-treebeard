e41c1f20df054848db48e0837feea594b06c1234
Alexander Naumtcev <38173482+naumtcev@users.noreply.github.com>
2020-08-26 15:47:28 +0700

    Update Drawer.jsx

--- src/components/TreeNode/Drawer.jsx
+++ src/components/TreeNode/Drawer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Drawer = ({children}) => children;
+const Drawer = ({ children }) => children;
 
 Drawer.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.func,
-        PropTypes.arrayOf(PropTypes.func, PropTypes.shape({})),
-        PropTypes.shape({})
-    ])
+  children: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.arrayOf(PropTypes.func, PropTypes.shape({})),
+    PropTypes.shape({}),
+  ]),
 };
 
 export default Drawer;
