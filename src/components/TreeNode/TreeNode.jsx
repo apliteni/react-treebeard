aa343a1e1da55d748c2d5a5ba48b659ca62a1773
Alexander Naumtcev <38173482+naumtcev@users.noreply.github.com>
2020-08-26 15:48:25 +0700

    Update and rename index.jsx to TreeNode.jsx

---
+++ src/components/TreeNode/TreeNode.jsx
@@ -0,0 +1,101 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { isArray, isFunction } from 'lodash';
+
+import { randomString } from '../../util';
+import { Ul } from '../common';
+import NodeHeader from '../NodeHeader';
+import Drawer from './Drawer';
+import Loading from './Loading';
+
+const Li = styled('li', {
+  shouldForwardProp: (prop) =>
+    ['className', 'children', 'ref'].indexOf(prop) !== -1,
+})(({ style }) => style);
+
+class TreeNode extends PureComponent {
+  onClick() {
+    const { node, onToggle } = this.props;
+    if (onToggle) {
+      onToggle(node, !node.toggled);
+    }
+  }
+
+  decorators() {
+    const { decorators, node } = this.props;
+    const nodeDecorators = node.decorators || {};
+
+    return { ...decorators, nodeDecorators };
+  }
+
+  renderChildren(decorators) {
+    const {
+      decorators: propDecorators,
+      node,
+      style,
+      onToggle,
+      onSelect,
+      customStyles,
+    } = this.props;
+
+    if (node.loading) {
+      return <Loading decorators={decorators} style={style} />;
+    }
+
+    let { children } = node;
+    if (!isArray(children)) {
+      children = children ? [children] : [];
+    }
+
+    return (
+      <Ul style={style.subtree}>
+        {children.map((child) => (
+          <TreeNode
+            onSelect={onSelect}
+            onToggle={onToggle}
+            style={style}
+            customStyles={customStyles}
+            decorators={propDecorators}
+            key={child.id || randomString()}
+            node={child}
+          />
+        ))}
+      </Ul>
+    );
+  }
+
+  render() {
+    const { node, style, onSelect, customStyles } = this.props;
+    const decorators = this.decorators();
+
+    return (
+      <Li style={style.base}>
+        <NodeHeader
+          decorators={decorators}
+          node={node}
+          style={style}
+          customStyles={customStyles}
+          onClick={() => this.onClick()}
+          onSelect={isFunction(onSelect) ? () => onSelect(node) : undefined}
+        />
+        <Drawer>{node.toggled ? this.renderChildren(decorators) : null}</Drawer>
+      </Li>
+    );
+  }
+}
+
+TreeNode.propTypes = {
+  onSelect: PropTypes.func,
+  onToggle: PropTypes.func,
+  style: PropTypes.object.isRequired,
+  customStyles: PropTypes.object,
+  node: PropTypes.object.isRequired,
+  decorators: PropTypes.object.isRequired,
+};
+
+TreeNode.defaultProps = {
+  customStyles: {},
+};
+
+export default TreeNode;
