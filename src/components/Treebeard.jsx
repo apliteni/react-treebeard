2e0bd174bd0643ea43bf36e38101d423627aff35
Alexander Naumtcev <38173482+naumtcev@users.noreply.github.com>
2020-08-26 15:46:20 +0700

    Update and rename index.jsx to Treebeard.jsx

---
+++ src/components/Treebeard.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { castArray } from 'lodash';
+
+import defaultTheme from '../themes/default';
+import { randomString } from '../util';
+import { Ul } from './common';
+import * as defaultDecorators from './Decorators/Decorators';
+import TreeNode from './TreeNode/TreeNode';
+
+const TreeBeard = ({
+  decorators,
+  data,
+  onToggle,
+  style,
+  onSelect,
+  customStyles,
+}) => (
+  <Ul style={{ ...defaultTheme.tree.base, ...style.tree.base }}>
+    {castArray(data).map((node) => (
+      <TreeNode
+        decorators={decorators}
+        node={node}
+        onToggle={onToggle}
+        onSelect={onSelect}
+        customStyles={customStyles}
+        key={node.id || randomString()}
+        style={{ ...defaultTheme.tree.node, ...style.tree.node }}
+      />
+    ))}
+  </Ul>
+);
+
+TreeBeard.propTypes = {
+  style: PropTypes.object,
+  customStyles: PropTypes.object,
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.array]).isRequired,
+  onToggle: PropTypes.func,
+  onSelect: PropTypes.func,
+  decorators: PropTypes.object,
+};
+
+TreeBeard.defaultProps = {
+  style: defaultTheme,
+  decorators: defaultDecorators,
+  customStyles: {},
+};
+
+export default TreeBeard;
